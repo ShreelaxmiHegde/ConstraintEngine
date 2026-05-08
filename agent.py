16d7549d6e4f6ae3c509b5fd1144d5e776cbcd4b
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2026-05-08 21:10:54 +0530

    add database configuration for chat context memory persistance

--- agent.py
+++ agent.py
@@ -1,7 +1,9 @@
 from agno.agent import Agent
 from agno.models.groq import Groq
 from agno.tools.duckduckgo import DuckDuckGoTools
+from agno.db.sqlite import SqliteDb
 from dotenv import load_dotenv
+from rich.pretty import pprint
 
 load_dotenv()
 
@@ -10,7 +12,25 @@ def build_agent():
   return Agent(
     model=Groq(id="qwen/qwen3-32b"),
     tools=[DuckDuckGoTools()],
+    description="You are a Database engineer expert in Database architectural decision making.",
+    instructions=["You should understand the situation, evaluate constraints, compare options, reason about tradeoffs and recommend best fit."],
+    db=SqliteDb(db_file="agent.db"),
+    add_history_to_context=True,
+    num_history_runs=3,
+    enable_user_memories=True,
     markdown=True,
-    instructions="You are an expert engineering decision maker with reasoning in database related confusions and development practices.",
-    add_datetime_to_context=True
+    add_datetime_to_context=True,
+    debug_mode=True
   )
+
+agent = build_agent()
+
+user_id = "shree@email.com"
+query = "What key factors to consider while choosing a right database."
+
+agent.print_response(query, user_id=user_id)
+
+memories = agent.get_user_memories(user_id)
+
+print("MEMORIES: ")
+pprint(memories)
