661d375430ef9d4a4ff3f7fce183af15dc12d6d8
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2026-05-28 21:25:18 +0530

    feat: user login and signup

--- backend/src/controller/user.controller.ts
+++ backend/src/controller/user.controller.ts
@@ -1,22 +1,63 @@
-import prisma from "../lib/prisma.js"
-import { Request, Response } from "express"
-import { SignUpBody } from "../types.js";
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+import { LoginBody, SignUpBody } from "../types.js";
+import { checkUser, createUser } from "../services/user.service.js";
+import { ExpressError } from "../utils/ExpressError.js";
 
+const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SALT = process.env.JWT_SALT;
 
-export const createUser = async (req: Request<{}, {}, SignUpBody>, res: Response) => {
-  const { username, email, password } = req.body;
+export const signUp = async (req: Request<{}, {}, SignUpBody>, res: Response) => {
+  console.log("Signup Request: ", req.body);
+  const { username, email, password } = req.body
 
-  const createUser = await prisma.user.create({
-    data: {
-      username: username,
-      email: email,
-      passwordHash: password
+  if (!JWT_SALT) throw new ExpressError("Missing JWT salt", 500);
+
+  const passwordHash = bcrypt.hashSync(password, JWT_SALT);
+
+  const registerUser = await createUser(username, email, passwordHash);
+
+  return res.status(200).json({
+    message: "User registered successfully",
+    user: {
+      username: registerUser.username,
+      email: registerUser.email
     }
-  })
+  });
+}
 
-  console.log(createUser);
+export const login = async (req: Request<{}, {}, LoginBody>, res: Response) => {
+  console.log("Login Request: ", req.body);
+  const { email, password } = req.body;
 
-  return res.json({
-    message: "User created successfully"
+  const user = await checkUser(email);
+
+  const isMatch = bcrypt.compareSync(password, user?.passwordHash);
+
+  if (!isMatch) throw new ExpressError("Invalid credentials", 401)
+  if (!JWT_SECRET) throw new ExpressError("Missing JWT secret", 500);
+
+  const token = jwt.sign(
+    {
+      userId: user.id,
+      email: user.email
+    },
+    JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+
+  res.cookie("access_token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000
+  });
+
+  return res.status(200).json({
+    success: true,
+    message: "Login was successful",
+    token
   });
 }
---
+++ backend/src/services/user.service.ts
@@ -0,0 +1,31 @@
+import prisma from "../lib/prisma.js"
+import { ExpressError } from "../utils/ExpressError.js";
+
+export const createUser = async (
+  username: string,
+  email: string,
+  passwordHash: string
+) => {
+
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      passwordHash
+    }
+  });
+
+  return user;
+}
+
+export const checkUser = async (email: string) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email
+    }
+  });
+
+  if (!user) throw new ExpressError("User not found", 404);
+
+  return user;
+}
