d402b8156ad1215636906045386ab52216e3908f
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2026-05-30 22:24:56 +0530

    config: types and constants

---
+++ backend/src/constants.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+
+export const NODE_ENV = process.env.NODE_ENV;
+export const fastapiURL = process.env.FASTAPI_URL;
+export const JWT_SALT = process.env.JWT_SALT;
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
+export const JWT_REFRESH_TOKEN = process.env.JWT_REFRESH_TOKEN;
+export const ACCESS_TTL = '15m';
+export const REFRESH_TTL_SEC = 60 * 60 * 24 * 7; // 7days
