b7d5b448c9e062caa3113f37703a1ee40649c568
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2026-06-01 17:13:49 +0530

    feat: remove header checks implementation for  access_token

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,22 +1,19 @@
 import jwt from "jsonwebtoken";
 import "dotenv/config";
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request } from "express";
 import { ExpressError } from "../utils/ExpressError.js";
+import { AuthPayload } from "../types/types.js";
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization || '';
-  const [scheme, tokenFromHeader] = authHeader.split(' ');
-  const tokenFromCookie = req.cookies?.access_token;
-  const token = scheme === 'Bearer' && tokenFromHeader ? tokenFromHeader : tokenFromCookie;
+export const authenticate = async (req: Request, next: NextFunction) => {
+  const token = req.cookies?.access_token;
 
-  if (scheme !== 'Bearer' || !token) throw new ExpressError("Missing or invalid authorization header", 401);
   if (!token) throw new ExpressError("Unauthorized", 401);
   if (!JWT_SECRET) throw new ExpressError("Missing JWT secret", 500);
 
   const decoded = jwt.verify(token, JWT_SECRET);
-  req.user = decoded;
+  req.user = decoded as AuthPayload;
 
   next();
 }
