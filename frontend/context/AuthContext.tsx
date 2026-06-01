19237bf8e058d4943752b69fb80b05ddfed88a29
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2026-06-01 16:58:07 +0530

    update: authcontext with auto user login on app start

--- frontend/context/AuthContext.tsx
+++ frontend/context/AuthContext.tsx
@@ -1,15 +1,32 @@
 "use client";
 
+import { getMe } from "@/api";
 import { AuthContextType, AuthProviderProps } from "@/types";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext<AuthContextType | null>(null);
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
   const [currUser, setCurrUser] = useState<AuthContextType["currUser"]>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const bootstrapAuth = async () => {
+      try {
+        const data = await getMe();
+        setCurrUser(data.user);
+      } catch {
+        setCurrUser(null);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    bootstrapAuth();
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ currUser, setCurrUser }}>
+    <AuthContext.Provider value={{ currUser, setCurrUser, loading, setLoading }}>
       {children}
     </AuthContext.Provider>
   );
