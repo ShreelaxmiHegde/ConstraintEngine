121d23c9b31e289d9a8961e2e7d0da28a3185b8f
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2026-06-11 20:07:53 +0530

    feat: build separate agents for extraction and architecting

--- ai_service/agent.py
+++ ai_service/agent.py
@@ -1,17 +1,15 @@
 from agno.agent import Agent
 from agno.models.groq import Groq
-from agno.tools.duckduckgo import DuckDuckGoTools
 from agno.db.sqlite import SqliteDb
 from dotenv import load_dotenv
-from rich.pretty import pprint
+from ai_service.schemas.extraction import ExtractConstraintOutput
 
 load_dotenv()
 
 
-def build_agent():
+def build_architect_agent():
   return Agent(
     model=Groq(id="qwen/qwen3-32b"),
-    tools=[DuckDuckGoTools()],
     description="You are a Database engineer expert in Database architectural decision making.",
     instructions=["You should understand the situation, evaluate constraints, compare options, reason about tradeoffs and recommend best fit."],
     db=SqliteDb(db_file="agent.db"),
@@ -23,14 +21,22 @@ def build_agent():
     debug_mode=True
   )
 
-agent = build_agent()
+def build_extraction_agent():
+  return Agent(
+    model=Groq(id="qwen/qwen3-32b"),
+    description="Extract requirements from software project descriptions.",
+    instructions="""
+      Extract:
+      - Functional requirements
+      - Non-functional requirements
+      - Technical constraints
+      - User roles
+      - Integrations
+      - Scale requirements
+      Identify ambiguities and generate clarification questions.
+    """,
+    output_schema=ExtractConstraintOutput
+  )
 
-user_id = "shree@email.com"
-query = "What key factors to consider while choosing a right database."
-
-agent.print_response(query, user_id=user_id)
-
-memories = agent.get_user_memories(user_id)
-
-print("MEMORIES: ")
-pprint(memories)
+extractor = build_extraction_agent()
+architect = build_architect_agent()
