c3c532dde287bea219edb6d995581602c71c85b5
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2026-06-11 20:14:20 +0530

    feat: implement basic API for constraint extraction & response generation

--- ai_service/main.py
+++ ai_service/main.py
@@ -1,18 +1,52 @@
+from agno.agent import RunOutputEvent, RunEvent
+from typing import Iterator
 from fastapi import FastAPI
 from pydantic import BaseModel
+from agent import extractor, architect
+from ai_service.schemas.extraction import ExtractConstraintOutput
 
 app = FastAPI()
 
-class Message(BaseModel):
-  message: str
+class Data(BaseModel):
+  query: str
+  user_id: int
+  session_id: int
 
-@app.get("/hello")
-def hello():
-  print("request recieved")
-  res = "hello from AI services"
-  return res
+class RawProject(BaseModel):
+  description: str
 
-@app.post("/c/{chat_id}")
-def message(message: Message):
-  print(message)
-  return {"response": f"You asked '{message.message}'"}
+@app.post("/extract_constraints")
+def extractConstraints(project: RawProject):
+  print(project.description)
+
+  response = extractor.run(project.description)
+  constraints: ExtractConstraintOutput = response.content
+  print("Model extracted constraints: ", constraints)
+
+  return {constraints}
+
+@app.post("/m")
+def generateResponse(d: Data):
+  print(d.query, d.user_id)
+  extractor.cli_app(stream=True)
+  
+  stream: Iterator[RunOutputEvent] = architect.run(
+    d.query, 
+    stream=True, 
+    stream_events=True, 
+    user_id=d.user_id,
+    session_id=d.session_id,
+    debug_mode=True
+  )
+
+  for chunk in stream:
+    if chunk.event == RunEvent.run_content:
+      print(f"Content: {chunk.content}")
+    elif chunk.event == RunEvent.tool_call_started:
+      print(f"Tool call started: {chunk.tool.tool_name}")
+    elif chunk.event == RunEvent.reasoning_step:
+      print(f"Reasoning step: {chunk.reasoning_content}")
+
+  # stream.content - full content
+
+  return {"response": stream}
