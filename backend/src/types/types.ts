12498dc508b0a2ae46e192d657a73bac3662787b
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2026-07-01 20:56:17 +0530

    add google/genai package

--- backend/src/types/types.ts
+++ backend/src/types/types.ts
@@ -3,18 +3,6 @@ export type conversationBody = {
   content: string
 }
 
-export type SignUpBody = {
-  username: string
-  email: string
-  password: string
-  guestId: string | null
-}
-
-export type LoginBody = {
-  email: string
-  password: string
-}
-
 export interface AuthPayload {
   userId: string
   email: string
